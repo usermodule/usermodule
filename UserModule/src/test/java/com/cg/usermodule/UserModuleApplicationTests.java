f79ae7ef58c8220dc6565af8732ed8f77709646e
usermodulecg@gmail.com <shravani.khadse@capgemini.com>
2023-01-30 12:10:04 +0530

    My Day 1 Sprint 1

---
+++ UserModule/src/test/java/com/cg/usermodule/UserModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.usermodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
