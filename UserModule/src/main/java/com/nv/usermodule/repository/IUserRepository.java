5c1fa5807b0d540cee15ad53a5675fe80cf9d52a
usermodule <123923600+usermodule@users.noreply.github.com>
2023-02-01 18:56:45 +0530

    Updated Repository

--- UserModule/src/main/java/com/nv/usermodule/repository/IUserRepository.java
+++ UserModule/src/main/java/com/nv/usermodule/repository/IUserRepository.java
@@ -1,13 +1,15 @@
 package com.nv.usermodule.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.nv.usermodule.entity.Address;
 import com.nv.usermodule.entity.User;
 
 @Repository
 public interface IUserRepository extends JpaRepository<User, Integer>{
 
 	
-
 }
